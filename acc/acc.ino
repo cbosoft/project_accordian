b40b0fba166035377482401ec13e2581a68a0dbf
cbosoft <kfb12157@uni.strath.ac.uk>
2018-02-04 14:54:46 +0000

    Added comment-header

--- acc/acc.ino
+++ acc/acc.ino
@@ -1,40 +1,52 @@
 #include <Keyboard.h>
+/**
+ * Project Accordian
+ * 
+ * See https://www.github.com/cbosoft/project_accordian
+ * 
+ * This work is licensed under CC-BY-NC-SA (https://creativecommons.org/licenses/by-nc-sa/3.0/).
+ * 
+ * This means do are free to use, alter, and pass along this work as long as it is not used for 
+ * commercial purposes, you give me some credit, and subsequent products have this (or a compatible)
+ * license.
+*/
 //#define DEBUG
-// see https://www.arduino.cc/reference/en/language/functions/usb/keyboard/keyboardmodifiers/
+
 const int PINS[18] = {
-   0,  1,  2,  3,  // LEFT (Index to Pinky)
+   0,  1,  2,  3,  // LEFT  (Index to Pinky)
    4,  6,  8, 10,  // RIGHT (Tips, Index to Pinky)
    5,  7,  9, 16,  // RIGHT (Mids, Index to Pinky)
-  A0, A1, A2, A3, A4, A5
+  A0, A1,          // MODS  (Left, Super + Shift)
+  A2, A3           // MODS  (Right, Alt + Ctrl)
   };
 const int KEYS[] = {                      // PR MI
-                                          // 00  -- top fingers released: navigation group
+                                          // 00  -- all fingers released: navigation group
   216, 218, 217, 215, 210, 213, 211, 214, // 00 00 // NAVIG1 // LEFT UP DOWN RIGHT   HOME END PGUP PGDN
   179, 176, 178, 212,  32, 177,   0,   0, // 00 01 // NAVIG2 // TAB RETURN BKSPC DEL   SPACE ESC
   194, 195, 196, 197, 198, 199, 200, 201, // 00 10 // FUNCS1 // F1 F2 F3 F4  F5 F6 F7 F8
   202, 203, 204, 205,   0,   0,   0,   0, // 00 11 // FUNCS2 // F9 F10 F11 F12
 
-                                          // 01  -- middle finger down, index released: alphabet group
+                                          // 01  -- ring finger down, pinky released: alphabet group
    97,  98,  99, 100, 101, 102, 103, 104, // 01 00 // ALPHA1 // ABCD EFGH
   105, 106, 107, 108, 109, 110, 111, 112, // 01 01 // ALPHA2 // IJKL MNOP
   113, 114, 115, 116, 117, 118, 119, 120, // 01 10 // ALPHA3 // QRST UVWX
   121, 122,   0,   0,   0,   0,   0,   0, // 01 11 // ALPHA4 // YZ
 
-                                          // 10  -- index finger down, middle released: math group
+                                          // 10  -- pinky finger down, ring released: math group
    48,  49,  50,  51,  52,  53,  54,  55, // 10 00 // MATHS1 // 0123 4567
    56,  57,   0,   0,   0,   0,   0,   0, // 10 01 // MATHS2 // 89
     0,   0,   0,   0,   0,   0,   0,   0, // 10 10 // --
     0,   0,   0,   0,   0,   0,   0,   0, // 10 11 // --
 
-                                          // 11  -- top fingers both down: symbols group
+                                          // 11  -- both fingers down: symbols group
    43,  45,  42,  47,  94,  61,   95,   0, // 11 00 // SYMBS1 // +-*/ ^=_
    40,  41,  37,  36,  60,  62,  33, 126, // 11 01 // SYMBS2 // ()%$ <>!~
    44,  46,  63,  34,  58,  59,  39,  35, // 11 10 // SYMBS3 //,.?" :;'#
    64,  38,  91,  93, 123, 125, 124,  92  // 11 11 // SYMBS4 // @&[] {}| (backslash)
   };
-const int MOD_KEYS[4] = {128, 129, 130, 131}; // CTRL SHIFT ALT SUPER
+const int MOD_KEYS[4] = {131, 129, 130, 128}; // SUPER SHIFT ALT CTRL
 
-int r_key = 0;
+int pressed_key = 0;
 int typed = 0;  
 bool pressed = false;
 int count = 0;
@@ -47,8 +59,17 @@ void setup() {
 }
 
 void loop() {
+  for (int i = 12; i < 18; i++){
+    if (!digitalRead(PINS[i])){
+      Keyboard.press(MOD_KEYS[i - 12]);
+    }
+    else{
+      Keyboard.release(MOD_KEYS[i - 12]);
+    }
+  }
+  
   if (pressed){
-    if (!digitalRead(PINS[r_key + 4])){
+    if (!digitalRead(PINS[pressed_key + 4])){
       delay(10); // prevents ghosting
       count += 1;
       if (count >= holddelay){
@@ -59,7 +80,7 @@ void loop() {
       }
     }
     else {
-      r_key = 0;
+      pressed_key = 0;
       typed = 0;
       count = 0;
       pressed = false;
@@ -68,11 +89,11 @@ void loop() {
   
   for (int i = 0; i < 8; i++){
     if (!digitalRead(PINS[4+i])){
-      r_key = i;
+      pressed_key = i;
       
       #ifdef DEBUG
       Serial.print(" Typed [");
-      Serial.print(r_key);
+      Serial.print(pressed_key);
       Serial.print("]\n");
       #endif
       
@@ -138,15 +159,7 @@ void loop() {
     Serial.write("\n");
     #endif
     
-    for (int i = 12; i < 18; i++){
-      if (!digitalRead(PINS[i])){
-        Keyboard.press(MOD_KEYS[i - 12]);
-      }
-      else{
-        Keyboard.release(MOD_KEYS[i - 12]);
-      }
-    }
-    typed = (group * 8) + r_key;
+    typed = (group * 8) + pressed_key;
 
     #ifdef DEBUG
     Serial.print("\n\tIndex ");
